ab48795eceefdea6e212ab443584c7910e4650ae
ycx <453277731@qq.com>
2021-12-08 15:30:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ycx
+ * @create 2021-12-08 15:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
